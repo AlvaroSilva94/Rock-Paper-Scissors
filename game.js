c5bbb02f420bfa6323f094d734bf3c9399e4a1f3
AlvaroSilva94 <alvaro.ma.silva@outlook.com>
2024-01-08 18:25:18 +0000

    Added some text elements to the html and functionality

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ let drawCount = 0;
 let playcount = 0;
 let comptcount = 0;
 let drawcount = 0;
-let playArray = 0;
+let playArray = [0,0,0];
 
 function resetCounters()
 {
@@ -14,7 +14,7 @@ function resetCounters()
  playcount = 0;
  comptcount = 0;
  drawcount = 0;
- playArray = 0;
+ playArray = [0,0,0];
 }
 
 function getComputerChoice() {
@@ -28,78 +28,69 @@ function getComputerChoice() {
 const Rock = document.getElementById('playRock');
 const Paper = document.getElementById('playPaper');
 const Scissors = document.getElementById('playScissors');
+const PlayerScore = document.getElementById('playerScore');
+const ComputerScore = document.getElementById('computerScore');
+const BothPicks = document.getElementById('bothPicks');
+const ResultInfo = document.getElementById('resultInfo');
 
 Rock.addEventListener("click", () => {handleSelections('rock');});
 Paper.addEventListener("click", () => {handleSelections('paper');});
 Scissors.addEventListener("click", () => {handleSelections('scissors'); });
 
 function handleSelections(player) {
+
+    if(playcount == 5 || comptcount == 5)
+    {
+        gameOver();
+    }
+
     compt = getComputerChoice();
     playArray = playRound(player,compt);
     playcount =+playArray[0];
     comptcount =+playArray[1];
     drawcount =+playArray[2];
-    game();
+    updateScoreLabels(playcount,comptcount);
+}
+
+function updateScoreLabels(playerScore, computerScore)
+{
+    PlayerScore.textContent = `Player: ${playerScore}`;
+    ComputerScore.textContent = `Computer: ${computerScore}`; 
 }
 
 function playRound(playerSelection, ComputerSelection){
 
     if(playerSelection == ComputerSelection)
     {
-        console.log(`Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`);
-        console.log("It's a tie, let's go again!")
+        BothPicks.textContent = `Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`;
+        ResultInfo.textContent = "It's a tie, let's go again!";
         drawCount++;
     }
 
     else{
-        if (playerSelection == "rock" && ComputerSelection == "paper")
+        if ((playerSelection == "rock" && ComputerSelection == "paper")||
+            (playerSelection == "paper" && ComputerSelection == "scissors") ||
+            (playerSelection == "scissors" && ComputerSelection == "rock"))
         {
-            console.log(`Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`);
-            console.log("Computer wins!");
+            BothPicks.textContent = `Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`;
+            ResultInfo.textContent = "Computer wins this round!";
             computerCount++;
         }
-        else if (playerSelection == "paper" && ComputerSelection == "scissors")
+        
+        else if ((playerSelection == "rock" && ComputerSelection == "scissors")||
+        (playerSelection == "paper" && ComputerSelection == "rock")||
+        (playerSelection == "scissors" && ComputerSelection == "paper"))
         {
-            console.log(`Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`);
-            console.log("Computer wins!");
-            computerCount++;
-        }
-        else if (playerSelection == "scissors" && ComputerSelection == "rock")
-        {
-            console.log(`Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`);
-            console.log("Computer wins!");
-            computerCount++;
-        }
-        else if (playerSelection == "rock" && ComputerSelection == "scissors")
-        {
-            console.log(`Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`);
-            console.log("Player wins!");
+            BothPicks.textContent = `Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`;
+            ResultInfo.textContent = "Player wins this round!";
             playerCount++;
-        }
-        else if (playerSelection == "paper" && ComputerSelection == "rock")
-        {
-            console.log(`Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`);
-            console.log("Player wins!");
-            playerCount++;
-        }
-        else if (playerSelection == "scissors" && ComputerSelection == "paper")
-        {
-            console.log(`Player chose ${playerSelection} and computer chose ${ComputerSelection}\n`);
-            console.log("Player wins!");
-            playerCount++;
-        }
     }
 
     return [playerCount,computerCount,drawCount];
+    }
 }
 
-function game(){ 
+function gameOver(){ 
 
-    console.log(`Player won ${playcount} times`);
-    console.log(`Computer won ${comptcount} times\n`);
-    console.log(`There was a draw ${drawcount} times`)
-
+    return playcount > comptcount ? (endgameMsg.textContent = 'You won!') : (endgameMsg.textContent = 'You lost...');    
 }
-
-//resetCounters();
-//game();
